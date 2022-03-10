c0baed9d442ebc840680935c2837ad0090e2a5fc
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-03-10 07:27:05 -0800

    commit 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{ printf("%dwith proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
