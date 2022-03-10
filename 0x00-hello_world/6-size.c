1e7edce45e7bf22c856921e2c252eb28cc2be0dd
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-03-10 09:43:11 -0800

    commit 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,11 @@ int main(void)
 	long int longInt;
 	long long int longLongInt;
 	char charSize;
+
 	printf("Size of a char: %zu byte(s)\n", sizeof(charSize));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intSize));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longInt));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongInt));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatSize));
-return (0);	
+return (0)	
 }
