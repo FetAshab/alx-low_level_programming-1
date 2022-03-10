dd0446cf9c254c2f90e03597d28a7d1c44d5fbc7
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-03-10 12:27:43 -0800

    commit 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string>
 /**
  * main - Entry point
  *
@@ -8,6 +9,5 @@ int main(void)
 { 
 	char str[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19"; 
 	write(2, str1, 59);
-
 	return (1);
 }
