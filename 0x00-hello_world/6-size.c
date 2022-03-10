6fa21b0563902dacfd7553d6fbda0bce578dd010
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-03-10 08:27:29 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,14 @@
 int main(void)
 {       int intSize;
 	float floatSize;
-	long intlongInteSize;
+	long int longInt;
 	long long int longLongInt;
 	char charSize;
 	printf("Size of a char: ",sizeof(charSize));
-	printf("Size of an int: ",sizeof(floatSize));
+	printf("Size of an int: ",sizeof(intSize));
 	printf("Size of a long int: ",sizeof(longInt));
         printf("Size of a long long int: ",sizeof(longLongInt));
-        printf("Size of a float: ",sizeof(charSize));
+        printf("Size of a float: ",sizeof(floatSize));
 return (0)	
 
 }
