4f022a09d51626a48ada30d2633d9d1696635035
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-03-10 08:10:40 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (sucess)
+ */
+int main(void)
+{       int intSize;
+	float floatSize;
+	long intlongInteSize;
+	long long int longLongInt;
+	char charSize;
+	printf("Size of a char: ",sizeof(charSize));
+	printf("Size of an int: ",sizeof(floatSize));
+	printf("Size of a long int: ",sizeof(longInt));
+        printf("Size of a long long int: ",sizeof(longLongInt));
+        printf("Size of a float: ",sizeof(charSize));
+return (0)	
+
+}
