db6dce2d54b335547780c8a92c79b306c7b1b7e1
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-03-11 06:00:00 -0800

    commit 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int lastNo = n%10;
-	printf("Last digit of %d is %d",n,lastNo);
+	printf("Last digit of %d is %d",n,n%10);
 	return (0);
 }
