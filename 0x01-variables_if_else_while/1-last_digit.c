062d3415a6e07171773ec2cc1e60db7110b5f9cd
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-03-11 06:20:54 -0800

    commit 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,11 +12,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is %d",n,n%10);
-         if (n > 5)
-	 { printf(" and is greater than 5\n"); }
+         if (n == 0)
+	 { printf(" and is 0"); }
 	else if (n < 6)
 	{ printf(" and is less than 6 and not 0\n"); }
 	 else
-	 { printf(" and is 0"); }
+	 {  printf(" and is greater than 5"); }
 			return (0);
 }
