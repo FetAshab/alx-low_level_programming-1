a4ca9ee4e3a783c1e536aefb0dad9bad1398ea10
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-03-11 06:48:45 -0800

    commit 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	 { printf(" and is 0\n"); }
 	else if ((n % 10) < 6)
 	{ printf(" and is less than 6 and not 0\n"); 
-	}else
+	} else
 	 {  printf(" and is greater than 5\n"); }
 			return (0);
 }
