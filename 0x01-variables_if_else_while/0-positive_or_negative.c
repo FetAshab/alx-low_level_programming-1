9937225b71c4538f575a54cba766b38e1b2d47e7
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-03-11 04:57:41 -0800

    commit 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,9 @@
 #include<stdlib.h>
 #include<time.h> 
 #include<stdio.h>
-/**
- * main - Entry point
- *
- * Return Always 0 (success)
- */
-int main(void)
-{
-		int n;
+/*betty style doc for function main goes there*/
+int main(void){
+int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
