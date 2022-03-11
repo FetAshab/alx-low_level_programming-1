8a08a9778b5d0eba10b9b0acb04551a705e08a38
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-03-11 11:44:44 -0800

    commit 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
-/*
- * main Entry point
- * return: Always 0 (success)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
  */
-int main(void){
+int main(void)
+{
 char small;
-while(small != q ||small !=e)
-{while(small <= z)
-	{putchar(small);
-	putchar("\n");
+while (small != 'q' || small != 'e')
+{
+	while (small <= 'z')
+	{
+		putchar(small);
+	putchar('\n');
 	small++;
 	}
 }
