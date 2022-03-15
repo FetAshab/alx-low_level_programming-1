e3020c717b9f5de97fc4e80a2b28d4aea0478580
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-03-15 11:47:51 -0400

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,13 @@
  * Return: Always 0
  */
 
-void print_alphabet(){
+void print_alphabet(void)
+{
 int i;
 
-for(i = 'a'; i <= 'z';i ++)
-{ _putchar(i);
+for (i = 'a'; i <= 'z'; i++)
+{ 
+ _putchar(i);
 }
  _putchar('\n');
 }
