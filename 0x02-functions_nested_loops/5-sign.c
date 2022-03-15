cacaec412d4fe7783b101079104990f6011b351a
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-03-15 18:57:24 -0400

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,20 +5,20 @@
  *@n: receives parameter n
  *Return: 1 for positive - for negative 0 for zero
  */
- int print_sign(int n)
- {
- if (n > 0)
- {
- _putchar('+');
- return (1);}
- else if (n < 0)
- {
- _putchar('-');
- return (-1);
- }
- else
- {
- _putchar('0');
- return (0);
- }
- }
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
