4cf91260bde977f989f0e6a8975f82917422c3ca
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-03-15 08:09:21 -0700

    commit 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,11 @@
-#include<stdio.h>
 #include "main.h"
 /**
  * main - print  putchar
  *
  * Return: Always 0
  */
-void print_alphabet(void);
-{
-	int i;
-
-	for (i = 'a' ; i <= 'z' ; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	return (0);
-}
 int main(void)
 {
-void print_alphabet(void);
+print_alphabet(void);
 return (0);
 }
