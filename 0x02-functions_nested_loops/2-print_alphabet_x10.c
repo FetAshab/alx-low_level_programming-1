bb5a08c33cfab5dd9b143f8a59aa43781cfc74f7
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-03-14 21:43:08 -0700

    commit 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - printing a-z 10x
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = a ; j <= z ; j++)
+		{
+		putchar(i);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
