31b969c9334e35a55550a28c15f3bbd36077ec49
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-03-14 21:05:00 -0700

    commit 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-/*
- * main - Nested loop
+/**
+ * main - printing putchar
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (sucess)
  */
 int main(void)
 {
-	printf("_putchar")
-		return (0);
+printf("_putchar");
+	return (0);
 }
