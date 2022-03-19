352b64a1b1b22428b0370cf15b8a2c77f201b262
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-03-19 00:04:06 -0400

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,16 @@
-include <stdio.h>                                                                                                                                                                     
-                                                                                                                                                                                       /**                                                                                                                                                                                    
- * main - Prints the alphabet. 
+#include <stdio.h>
+
+/**
+ * main - Entry point
  *
- * Return: Always 0 (success)
- */                                                                 
-int main(void)                                                                                                                                                                         
-{                                                                                                                                                                                       	        char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";                                                                                                                         int i;                                                                                                                                                                 for (i = 0; i < 52; i++)                                                                                                                                                       
-        {                                                                                                                                                                              
-        putchar(alp[i]);                                                                                                                                                               
-        }                                                                                                                                                                              
-        putchar('\n');                                                                                                                                                                 
-               return (0);                                                                                                                                                            
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+	putchar(a);
+putchar('\n');
+
+return (0);
 }
