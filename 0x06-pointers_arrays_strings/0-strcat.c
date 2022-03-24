619b04f4931b06e6f5b3a0db03c664384cf13e24
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-03-24 12:36:57 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,6 @@
-#include<stdio.h>
 #include "main.h"
 /**
- *_strcat - concatinate two string
+ *_strcat - concatinate two string,
  *@dest: the destination
  *@src: the source
  *Returns: concatinated String
