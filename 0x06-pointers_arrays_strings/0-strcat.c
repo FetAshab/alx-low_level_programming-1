beb8a39aa90aa19cd561f169971b37a86280be01
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-03-24 12:33:01 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 #include "main.h"
 /*
- *_strcat(char *dest, char *src) - concatinate two string
+ *_strcat - concatinate two string
  *@dest: the destination
  *@src: the source
  *Returns: concatinated String
  */
 char *_strcat(char *dest, char *src)
 {
-   int i=0,j=0;
+   int i = 0, j = 0;
    
 while (dest[i] != '\0')
 i++;
@@ -18,6 +18,6 @@ while (src[j] != '\0')
 i++;
 j++;
 }
-dest[i] ='\0';
+dest[i] = '\0';
 return (dest);
 }
