edb204f49d8de2995b9416a542034384a328e204
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-03-28 16:46:24 -0400

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,16 @@
-char *_memset(char *s, char b, unsigned int n);
+#include "main.h"
+/**
+ *_memcpy - copys the array
+ *@dest: recives the array
+ *@src: sends the array
+ *@n: how much of it
+ *Returns: dest
+ */
+void *_memcpy(char *dest,char *src,unsigned int n)
 {
 for(int i=0;i<n;i++)
 {
-s[i]=b;
+dest[i]=src[i];
 }
+return (dest);
 }
