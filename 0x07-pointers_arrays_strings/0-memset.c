ff0a79fc0e2489bdea01e39cf421a1e298e39154
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-03-28 16:12:01 -0400

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ *_memset - Assigns number of arrays by some variable
+ *@s: parameter to store the array name
+ *@b: parameter to store the value to be assigned
+ *@n: parameter to store how much value is assigned
+ *Return: the assigned array
+ */
 char *_memset(char *s, char b, unsigned int n);
 {
 for(int i=0;i<n;i++)
