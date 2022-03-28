9f78e838b0325936db0c8984dfd0d0c00f16ab21
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-03-28 16:49:50 -0400

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *_memcpy - copys the array
- *@dest: recives the array
- *@src: sends the array
- *@n: how much of it
- *Returns: dest
+ * _memcpy - copys the array
+ * @dest: recives the array
+ * @src: sends the array
+ * @n: how much of it
+ * Returns: dest
  */
 void *_memcpy(char *dest,char *src,unsigned int n)
 {
