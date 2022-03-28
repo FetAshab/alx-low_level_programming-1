7ddc8162802ac1c428840a71dc2e0b701b76c60e
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-03-28 16:12:59 -0400

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *_memset - Assigns number of arrays by some variable
- *@s: parameter to store the array name
- *@b: parameter to store the value to be assigned
- *@n: parameter to store how much value is assigned
- *Return: the assigned array
+ * _memset - Assigns number of arrays by some variable
+ * @s: parameter to store the array name
+ * @b: parameter to store the value to be assigned
+ * @n: parameter to store how much value is assigned
+ * Return: the assigned array
  */
 char *_memset(char *s, char b, unsigned int n);
 {
