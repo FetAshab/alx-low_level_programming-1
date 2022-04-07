e89a618cf7e2da7a3315a19eddbb8ed06328865c
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-04-07 13:15:32 -0400

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * malloc_checked - exit 98if null
+ * malloc_checked - allocates memory
  * @b: parmaetr passed
  * Return: the pointer if not null 
  * if mallloc fails, Return value 98
