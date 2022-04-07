9445fb9c202a8d982cdaa005198ea3569c19daf3
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-04-07 09:59:46 -0700

    commit 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ * malloc_checked - exit 98if null
+ * @b: parmaetr passed
+ * Return: the parameter if not null 
+ */
+void *malloc_checked(unsigned int b)
+{
+	    char *c;
+
+	c = malloc(b);
+	if (c == NULL)
+	exit(98);
+	return c;
+
+}
