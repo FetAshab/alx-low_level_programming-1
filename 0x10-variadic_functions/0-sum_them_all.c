040131e5b697dbb78f04933935fe839972335a60
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-04-14 10:25:16 -0400

    Create 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+/**
+ *sum_them_all - sum all the parmaeter.
+ *@n: passed parameter size
+ *return: 0 if n = 0 else sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+    if (n == 0)
+    return 0;
+    else
+    {
+    va_list ap;
+
+    va_start (ap ,n);
+
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+
+    sum += va_arg(ap,int);
+    }
+    return sum;
+
+    va_end(ap);
+    }
+
+    }
