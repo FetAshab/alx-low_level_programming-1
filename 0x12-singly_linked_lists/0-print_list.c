584957cdf069aa941187dcac81dee87e1ce4d0be
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-04-21 10:41:52 -0400

    Create 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_list - prints the list and the size.
+ *@h: passed list.
+ *returns: the the nodes.
+ */
+
+size_t print_list(const  list_t *h)
+{int i, char *s;
+  while (h)
+{
+  if (!h)
+  printf ("[%d] %s", 0, "nil");
+  else
+  printf("[%d] %s", h->len, str);
+  h = h->next;
+  i++;
+}
+  return i;
+}
