81fb2cb18d0771e6ebc4deb42a066acc7b77e92b
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-04-21 13:26:22 -0400

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *list_len - counts the item.
+ *@h: the item.
+ *
+ *Return: the size.
+ */
+
+size_t list_len(const list_t *h)
+{int i = 0;
+while (h)
+{
+if(h->str)
+h = h->next;
+i++;
+}
+return (i);
+}
