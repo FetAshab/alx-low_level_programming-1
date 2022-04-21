59e7e37d26f3e0c7279429de59a4421f737b7093
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-04-21 13:01:19 -0400

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,18 +5,19 @@
  *
  *Return: the the nodes.
  */
-size_t print_list(const  list_t *h)
+size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t s = 0;
 
 	while (h)
 	{
-	if (!h->str)
-		printf("[0] (nil)\n");
-	else
-		printf("[%u] %s\n", h->len, h->str);
-	h = h->next;
-	i++;
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
 	}
-	return (i);
+
+	return (s);
 }
