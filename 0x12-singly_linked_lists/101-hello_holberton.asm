1c21f906eb9e948a9cd135f66f50925c0538e75a
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-04-23 15:35:41 -0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,12 +6,13 @@ mov eax, 0x04
 mov ebx, 1
 mov ecx, message
 mov edx, message_length
+extern printf
 int 0x80
 
 mov eax, 0x01
 mov ebx, 0
 int 0x080
 selection .data:
- message: db "Hello, Holberton"
- message_length equ $-message
+ msg: db "Hello, Holberton"
+call printf
 
