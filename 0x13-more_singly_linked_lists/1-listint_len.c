ca4ce11b91ae5a11adf6c3c6a0b7497d8fd44ff8
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-04-25 13:46:48 -0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,7 +4,7 @@
  *listint_len - returns the nodes.
  *@h: parameter passed(head).
  *
- *return: the number of the nodes.
+ *Return: the number of the nodes.
  */ 
 size_t listint_len(const listint_t *h)
 {
